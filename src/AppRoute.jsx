46637f9ab57e470a8f4fe378ad410da3269a32e3
akashkrishnan <akashkrishnan806@gmail.com>
2026-02-08 22:07:03 +0530

    Bug Clear 2 Commit

---
+++ src/AppRoute.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Home from "./components/Home";
+import LoveYou from "./components/yes";
+
+
+function AppRoute() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/yes" element={<LoveYou />} />
+
+        {/* Redirect any unknown route to Home */}
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default AppRoute;
