93f0ab443833e9b968c2f9a8aec8fb2f6ad554ea
akashkrishnan <akashkrishnan806@gmail.com>
2026-02-08 21:58:19 +0530

    Bug Clear Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
-
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./components/Home";
 import LoveYou from "./components/yes";
 
-// import "./App.css";
 
-export default function App() {
+function App() {
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/yes" element={<LoveYou />} />
-        </Routes>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/yes" element={<LoveYou />} />
+
+        {/* Redirect any unknown route to Home */}
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
+
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+/* Guaranteed dev fix: always go to Home on refresh */
+if (window.location.pathname !== "/") {
+  window.location.replace("/");
+}
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
